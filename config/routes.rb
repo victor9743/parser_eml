41d01adf053b87ed2501376ea3b4216ab532e203
Victor Costa <victorcosta9743@gmail.com>
2025-11-29 09:31:12 -0300

    correction bootstrap errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
-  root 'incoming_emails#new'
-  resources :incoming_emails, only: [:new, :create, :index, :show] do
-    member do
-      post :reprocess
-    end
-  end
+  root 'incoming_emails#index'
+  resources :incoming_emails, only: [:new, :create, :index, :show]
   resources :customers, only: [:index, :show]
   resources :email_processings, only: [:index, :show]
 
